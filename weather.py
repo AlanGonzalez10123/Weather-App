403e47874d42ca9ddebd8a22738c1fc9b9fd43f5
AlanGonzalez10123 <alangonzalezosorio04@gmail.com>
2024-09-01 13:07:11 -0400

    Basic functionality

--- weather.py
+++ weather.py
@@ -1,7 +1,23 @@
 from flask import Flask
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
 
-app = Flask(__name__)
+load_dotenv()
 
-@app.route('/')
-def weather():
-    return "Current weather is: "
+def weather(city="Alexandria"):
+    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+
+    weather = requests.get(request_url).json()
+
+    return weather
+
+if __name__ == "__main__":
+    print('\n***Get Current Weather***\n')
+    city = input("\nEnter a city name: ")
+
+    weather_data = weather(city)
+
+    print("\n")
+    pprint(weather_data)
